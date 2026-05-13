a08bdcad83f20e2f0e8579c1da64655d3fb3f788
Andre Luiz Pinheiro Lopes <andreprodat97@gmail.com>
2026-05-12 22:00:36 -0300

    funcionalidade buscar

--- codigo/public/modulos/sprint1_andre/style.js
+++ codigo/public/modulos/sprint1_andre/style.js
@@ -1,1 +1,56 @@
+let estoque = [];
+
+const botaoBuscar = document.getElementById('buscar');
+const pesquisa = document.getElementById('pesquisa') ;
+const tabela = document.getElementById('tabela-estoque');
+
+
 fetch("http://localhost:3000/estoque")
+    .then(resposta => resposta.json())
+    .then(dados => {
+
+        estoque = dados;
+        console.log("estoque carregado: " , estoque);
+
+    });
+
+
+
+botaoBuscar.addEventListener('click', function(e){
+
+    const alimento = pesquisa.value.toLowerCase();
+     
+    const encontrado = estoque.filter(item => item.nome.toLowerCase().includes(alimento)
+    );    
+   
+   tabela.innerHTML = "";
+
+   if (encontrado.length > 0){
+     encontrado.forEach(encontrado => {
+        
+    const novaLinha = document.createElement('tr');
+
+    novaLinha.innerHTML = ` 
+    <td> ${encontrado.nome}</td>
+    <td>${encontrado.quantidade}</td>
+    <td>${encontrado.unidade}</td>
+
+    `;
+    tabela.appendChild(novaLinha);
+     });    
+     
+    }
+    else{ alert("Alimentado não encontrado");
+
+    } 
+
+    pesquisa.value = ""; 
+
+});
+
+
+    
+
+
+
+
