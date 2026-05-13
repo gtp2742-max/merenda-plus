86b8b0f91af588467c2f2698c4729d36687a4c06
Gustavo <galvescsousa@gmail.com>
2026-05-13 20:21:17 -0300

    Add botao remover e adicionar funcionando visualmente

--- codigo/public/modulos/Sprint1_Gustavo/script.js
+++ codigo/public/modulos/Sprint1_Gustavo/script.js
@@ -9,17 +9,25 @@ fetch("http://localhost:3000/preparos")
 const listaAlimentos = document.getElementById("listaAlimentos")
 
 const btnMostrar = document.getElementById("btnMostrar");
+
 const areaAdicionar =
         document.getElementById("areaAdicionar");
-    btnMostrar.addEventListener("click", () => {
+        btnMostrar.addEventListener("click", () => {
         areaAdicionar.style.display = "block";
     })
+
 const botao = document.getElementById("btnAdicionar");
 botao.addEventListener("click", () => {
 
     const nome = document.getElementById("nomeAlimento").value;
     const quantidade = document.getElementById("quantidadeAlimento").value;
     const lista = document.getElementById("listaAlimentos");
+    const novoAlimento = {
+        nome: nome,
+        quantidade: quantidade
+    };
+
+    dados[0].alimentos.push(novoAlimento);
 
     lista.innerHTML+= `
     
@@ -34,7 +42,19 @@ botao.addEventListener("click", () => {
                 ${quantidade}kg
             </div>
 
+<div class="col-2 text-center">
+
+    <button
+        class="btn btn-danger btn-sm"
+        onclick="this.closest('.row').remove()">
+
+        Remover
+
+    </button>
+
+</div>
         </div>
+        
     `;
 
     document.getElementById("nomeAlimento").value = "";
@@ -45,7 +65,7 @@ botao.addEventListener("click", () => {
 })
 
 
-dados[0].alimentos.forEach(alimento => {
+dados[0].alimentos.forEach((alimento, index) => {
     listaAlimentos.innerHTML += `
         <div class ="row border-bottom p-2">
 
@@ -56,9 +76,38 @@ dados[0].alimentos.forEach(alimento => {
             <div class="col-6 text-center">
             ${alimento.quantidade}kg
             </div>
+
+            <div class="col-2 text-center">
+
+            <button
+            class="btn btn-danger btn-sm"
+            onclick="this.closest('.row').remove()">
+
+             Remover
+
+            </button>
+            </div>
         </div>
 
         `;
 
+    });
+
+    const btnSalvar = 
+        document.getElementById("btnSalvar");
+
+    btnSalvar.addEventListener("click", () => {
+
+        fetch(`http://localhost:3000/preparos/${dados[0].id}`, {
+            method: "PUT",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dados[0])
+        })
+        .then(() => {
+            alert("Preparo salvo com sucesso!");
+        });
     });
 });
