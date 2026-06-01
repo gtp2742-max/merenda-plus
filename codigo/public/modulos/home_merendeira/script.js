d5923e3f11e3d2b432fa506fa8e7db27ce94a0af
davicarneiro1558195-sys <davi.carneiro.1558195@sga.pucminas.br>
2026-06-01 17:51:41 -0300

    Começo do home da merendeira.

---
+++ codigo/public/modulos/home_merendeira/script.js
@@ -0,0 +1,76 @@
+const apiUrl = "http://localhost:3000/consumos";
+
+
+
+
+function createConsumo(consumo) {
+
+    fetch(apiUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(consumo)
+    })
+        .then(() => {
+            alert("Consumo inserido com sucesso");
+        });
+}
+
+
+function desperdicio(){
+    let porcoes = Number(document.getElementById("porcoes").value)
+    let sobras = Number(document.getElementById("sobras").value)
+
+    const valor = ( sobras / porcoes) * 100
+    return valor
+}
+
+let qtdservida = document.getElementById("qtdservida")
+let porcoes = document.getElementById("porcoes")
+let sobras = document.getElementById("sobras");
+
+porcoes.addEventListener("input", function () {
+    
+    sobras.value = Number(porcoes.value) - Number(qtdservida.value);
+    document.getElementById("desperdicio").value = desperdicio().toFixed(0);
+});
+sobras.addEventListener("input", function () {
+
+    document.getElementById("desperdicio").value = desperdicio().toFixed(0);
+});
+
+qtdservida.addEventListener("input", function () {
+
+    sobras.value = Number(porcoes.value) - Number(qtdservida.value);
+    document.getElementById("desperdicio").value = desperdicio().toFixed(0);
+});
+
+document.getElementById("btnSalvar")
+.addEventListener("click", function () {
+            let form1 = document.getElementById("form1");
+            let form2 = document.getElementById("form2");
+
+            if (!form1.checkValidity() && !form2.checkValidity()) {
+
+                alert("Preencha os formulários");
+                return;
+            }
+
+            let consumos = {
+
+                id: "",
+
+                refeicaoDoDia: document.getElementById("refeicao_dia").value,
+                data: document.getElementById("data").value,
+                refeicaoTipo: document.querySelector('input[name="tipo"]:checked').value,
+                quantidadeServida: Number(document.getElementById("qtdservida").value),
+                quantidadeSobra: Number(document.getElementById("sobras").value)
+            };
+
+            createConsumo(consumos);
+
+            form1.reset();
+            form2.reset();
+
+        });
