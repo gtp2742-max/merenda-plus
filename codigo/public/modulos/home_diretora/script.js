081579cf8264cfa13eb3dee60ce7e93b1601f0a5
davicarneiro1558195-sys <davi.carneiro.1558195@sga.pucminas.br>
2026-06-18 15:57:32 -0300

    Acrecentando começo do home da diretora.

---
+++ codigo/public/modulos/home_diretora/script.js
@@ -0,0 +1,6 @@
+const apiUsuarios = "http://localhost:3000/usuarios";
+const apiEstoque = "http://localhost:3000/estoque";
+const apiConsumos = "http://localhost:3000/consumos";
+const apiRelatorios = "http://localhost:3000/retalorios";
+const apiProblemas = "http://localhost:3000/estoque";
+
