eb3664173d9197b58a27e3e671099a56222c9547
davicarneiro1558195-sys <davi.carneiro.1558195@sga.pucminas.br>
2026-06-18 18:11:24 -0300

    adicionanando editar e deletar do  registrar consumo

--- codigo/public/modulos/registrar_consumo/script.js
+++ codigo/public/modulos/registrar_consumo/script.js
@@ -1,7 +1,41 @@
 const apiUrl = "http://localhost:3000/consumos";
 
 
+async function MostrarInformacoes() {
+    const resposta = await fetch(apiUrl)
+    const data = await resposta.json()
 
+    if (data) {
+        let textoHTML = `
+                <tr>
+                <th>ID</th>
+                <th>Refeição do Dia</th>
+                <th>Data</th>
+                <th>Tipo</th>
+                <th>Quantidade Servida</th>
+                <th>Quantidade Sobra</th>
+                </tr>
+            `
+        for (let index = 0; index < data.length; index++) {
+            const dat = data[index]
+            textoHTML += `
+                        <tr>
+                        <td>${dat.id}</td>
+                        <td>${dat.refeicaoDoDia}</td>
+                        <td>${dat.data}</td>
+                        <td>${dat.refeicaoTipo}</td>
+                        <td>${dat.quantidadeServida}</td>
+                        <td>${dat.quantidadeSobra}</td>
+                        </tr>
+                        
+        `
+        }
+
+        document.querySelector("#tabela").innerHTML = `
+                    ${textoHTML}
+    `
+    } 
+}
 
 function createConsumo(consumo) {
 
@@ -17,6 +51,29 @@ function createConsumo(consumo) {
         });
 }
 
+function updateConsumo(id, consumo) {
+
+    fetch(`${apiUrl}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(consumo)
+    })
+        .then(() => {
+            alert("Consumo alterado");
+        });
+}
+
+function deleteConsumo(id) {
+
+    fetch(`${apiUrl}/${id}`, {
+        method: "DELETE"
+    })
+        .then(() => {
+            alert("Consumo removido");
+        });
+}
 
 function desperdicio(){
     let porcoes = Number(document.getElementById("porcoes").value)
@@ -45,6 +102,31 @@ qtdservida.addEventListener("input", function () {
     sobras.value = Number(porcoes.value) - Number(qtdservida.value);
     document.getElementById("desperdicio").value = desperdicio().toFixed(0);
 });
+let idConsumo
+
+document.getElementById("tabela")
+        .addEventListener("click", function (e) {
+
+            if (e.target.tagName == "TD") {
+
+                let linha = e.target.parentNode.querySelectorAll("td");
+
+                idConsumo = linha[0].innerText
+                document.getElementById("refeicao_dia").value = linha[1].innerText;
+                document.getElementById("data").value = linha[2].innerText;
+                let refeicao = linha[3].innerText;
+                document.getElementById("qtdservida").value = Number(linha[4].innerText);
+                document.getElementById("sobras").value = Number(linha[5].innerText);
+
+                 document.querySelector(
+                `input[name="tipo"][value="${refeicao}"]`
+                ).checked = true;
+
+                document.getElementById("porcoes").value = Number(linha[4].innerText) + Number(linha[5].innerText);
+            }
+
+           
+        });
 
 document.getElementById("btnSalvar")
 .addEventListener("click", function () {
@@ -74,3 +156,88 @@ document.getElementById("btnSalvar")
             form2.reset();
 
         });
+
+document.getElementById("btnAlterar")
+.addEventListener("click", function () {
+            let form1 = document.getElementById("form1");
+            let form2 = document.getElementById("form2");
+
+            if (!form1.checkValidity() && !form2.checkValidity()) {
+
+                alert("Preencha os formulários");
+                return;
+            }
+
+            let consumos = {
+
+                id: "",
+
+                refeicaoDoDia: document.getElementById("refeicao_dia").value,
+                data: document.getElementById("data").value,
+                refeicaoTipo: document.querySelector('input[name="tipo"]:checked').value,
+                quantidadeServida: Number(document.getElementById("qtdservida").value),
+                quantidadeSobra: Number(document.getElementById("sobras").value)
+            };
+
+            updateConsumo(idConsumo,consumos);
+
+            form1.reset();
+            form2.reset();
+
+            document.getElementById("btnAlterar")
+            .classList.toggle("d-none");
+
+            document.getElementById("btnDeletar")
+            .classList.toggle("d-none");
+
+            document.getElementById("consumos")
+            .classList.toggle("d-none");
+
+            document.getElementById("btnEditar")
+            .classList.remove("d-none");
+
+        });
+
+document.getElementById("btnEditar")
+.addEventListener("click", async function () {
+
+        document.getElementById("btnAlterar")
+            .classList.remove("d-none");
+
+        document.getElementById("btnDeletar")
+            .classList.remove("d-none");
+
+        document.getElementById("consumos")
+            .classList.remove("d-none");
+
+        document.getElementById("btnEditar")
+            .classList.toggle("d-none");
+            
+        
+    });
+
+document.getElementById("btnDeletar")
+        .addEventListener("click", function () {
+
+            let form1 = document.getElementById("form1");
+            let form2 = document.getElementById("form2");
+
+            deleteConsumo(idConsumo);
+
+            form1.reset();
+            form2.reset();
+
+            document.getElementById("btnAlterar")
+            .classList.toggle("d-none");
+
+            document.getElementById("btnDeletar")
+            .classList.toggle("d-none");
+
+            document.getElementById("consumos")
+            .classList.toggle("d-none");
+
+            document.getElementById("btnEditar")
+            .classList.remove("d-none");
+        });
+
+MostrarInformacoes();
