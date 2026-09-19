8c68e7c1b61c881f4787061816620a2e72546129
gtp2742-max <gtp2742@gmail.com>
2026-09-18 21:04:10 -0300

    docs: update JSON Server source attribution

--- codigo/index.js
+++ codigo/index.js
@@ -1,24 +1,14 @@
-// Trabalho Interdisciplinar 1 - Aplicações Web
+// Merenda+ - Trabalho Interdisciplinar 1
+// Engenharia de Software - PUC Minas
 //
-// Esse módulo implementa uma API RESTful baseada no JSONServer
-// O servidor JSONServer fica hospedado na seguinte URL
-// https://jsonserver.rommelpuc.repl.co/contatos
-//
-// Para montar um servidor para o seu projeto, acesse o projeto 
-// do JSONServer no Replit, faça o FORK do projeto e altere o 
-// arquivo db.json para incluir os dados do seu projeto.
-//
-// URL Projeto JSONServer: https://replit.com/@rommelpuc/JSONServer
-//
-// Autor: Rommel Vieira Carneiro
-// Data: 03/10/2023
+// Servidor da aplicação baseado em JSON Server.
+// Estrutura inicial adaptada do material acadêmico fornecido por
+// Rommel Vieira Carneiro (2023).
 
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./db/db.json')
-  
-// Para permitir que os dados sejam alterados, altere a linha abaixo
-// colocando o atributo readOnly como false.
+
 const middlewares = jsonServer.defaults({
   static: "./public",
   noCors: true });
